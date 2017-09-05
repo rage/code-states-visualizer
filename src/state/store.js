8205eae28ac3ee738a0a6101e3ce2b35117a4ec1
Henrik Nygren <nygrenh@gmail.com>
2017-09-05 08:09:20 +0300

    Add redux action analytics

--- src/state/store.js
+++ src/state/store.js
@@ -1,6 +1,9 @@
 // @flow
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
+import ReduxActionAnalytics from 'redux-action-analytics';
+import createHash from 'sha.js';
+import * as storejs from 'store';
 import rootReducer from './reducer';
 
 export type ThunkArgument = {
@@ -10,10 +13,24 @@ export default function makeStore(input: string) {
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   /* eslint-enable no-underscore-dangle */
+  const sha256 = createHash('sha256');
+  const identifier = sha256.update(input, 'utf8').digest('hex');
+  const analytics = new ReduxActionAnalytics('https://usage.testmycode.io/api/v0/data', 'code-states-visualizer', identifier, 10000, () => {
+    const user = storejs.get('tmc.user');
+    if (user === undefined) {
+      return {};
+    }
+    return {
+      username: user.username,
+    };
+  });
   const store = createStore(
     rootReducer(input),
     composeEnhancers(
-      applyMiddleware(thunk.withExtraArgument()),
+      applyMiddleware(
+        analytics.getMiddleware(),
+        thunk.withExtraArgument(),
+      ),
     ),
   );
   return store;
